d025c071996131ef0053044daad83dd884cbc76f
Brice Duke <bricewduke@gmail.com>
2021-05-02 15:27:09 -0500

    🏗️ [Bravado]: Migrate to postgres

---
+++ bravado/src/constants.ts
@@ -0,0 +1,3 @@
+require('dotenv').config();
+
+export const __prod__ = process.env.NODE_ENV !== 'production';
--- bravado/src/server.ts
+++ bravado/src/server.ts
@@ -1,39 +1,13 @@
-import 'reflect-metadata';
+import { MikroORM } from '@mikro-orm/core';
 
-import { ApolloServer } from 'apollo-server-express';
-import * as Express from 'express';
-import * as Mongoose from 'mongoose';
-import { buildSchema } from 'type-graphql';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
 
 (async function () {
-  require('dotenv').config();
-
-  const { MONGO_URI, SERVER_URI, PORT } = process.env;
-
-  const schema = await buildSchema({
-    resolvers: [''],
-    emitSchemaFile: true,
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: 'polyrant',
+    type: 'postgresql',
+    debug: !__prod__,
   });
-
-  const app = Express();
-
-  Mongoose.connect(MONGO_URI as string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => {
-      console.log('[Database]: Connected!');
-
-      const server = new ApolloServer({
-        schema,
-        context: () => ({}),
-      });
-
-      server.applyMiddleware({ app });
-
-      app.listen(PORT, () => {
-        console.log(`[Server]: Running at ${SERVER_URI}:${PORT}`);
-      });
-    })
-    .catch((err) => console.error(err));
 })();
