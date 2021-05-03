995723c3b4110154b42bf9fcccbe4b43cf20ed7f
Brice Duke <bricewduke@gmail.com>
2021-05-02 23:04:36 -0500

    🏗️ [Bravado]: Add GraphQL

--- bravado/src/server.ts
+++ bravado/src/server.ts
@@ -1,9 +1,30 @@
+require('dotenv').config();
+
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
 import { MikroORM } from '@mikro-orm/core';
+import { buildSchema } from 'type-graphql';
 
 import ormConfig from '../mikro-orm.config';
+import { PingResolver } from './resolvers/ping';
 
 (async function () {
-  const orm = await MikroORM.init(ormConfig);
+  const { SERVER_URI, PORT } = process.env;
 
+  const orm = await MikroORM.init(ormConfig);
   orm.getMigrator().up();
-})();
+
+  const app = express();
+
+  const apollo = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PingResolver],
+    }),
+  });
+
+  apollo.applyMiddleware({ app });
+
+  app.listen(2003, () => {
+    console.log(`[Server]: Started on ${SERVER_URI}:${PORT}/graphql`);
+  });
+})().catch((err) => console.error(err));
